a33d1be66c0c625f50e5a74f1e9c18bd043bd598
Alexander Fadeev <fadeevab.com@gmail.com>
2024-05-27 15:55:37 +0300

    Implement std::error::Error for cocoon::Error via optional `thiserror` (#28)
    This adds the `std::error::Error` trait implementation to
    `cocoon::Error` using the `thiserror` crate.
    Note that this is only limited to the presence of feature "thiserror".
    
    Co-authored-by: Alexander Fadeev <fadeevab.com@gmail.com>
    Co-authored-by: madomado <madonuko@outlook.com>

--- src/error.rs
+++ src/error.rs
@@ -1,20 +1,39 @@
 /// Error variants produced by the Cocoon API.
 #[derive(Debug)]
+#[cfg_attr(feature = "thiserror", derive(thiserror::Error))]
 pub enum Error {
     /// I/o error during read/write operation (`Cocoon::dump`, `Cocoon::parse`).
     #[cfg(feature = "std")]
+    #[cfg_attr(feature = "thiserror", error("Input/output error"))]
     Io(std::io::Error),
+
     /// Format is not recognized. Probably corrupted.
+    #[cfg_attr(feature = "thiserror", error("Unrecognized format"))]
     UnrecognizedFormat,
+
     /// Cryptographic error. There could be a few reasons:
     /// 1. Integrity is compromised.
     /// 2. Password is invalid.
+    #[cfg_attr(
+        feature = "thiserror",
+        error("Cryptographic error: bad integrity/password")
+    )]
     Cryptography,
+
     /// Container is too large to get processed on the current architecture.
     /// E.g. it's not possible to process a container larger than 4 GB on 32-bit architecture.
+    #[cfg_attr(
+        feature = "thiserror",
+        error("Container size exceeds architectural limit")
+    )]
     TooLarge,
+
     /// Buffer is too short and barely holds all data to decrypt, inconsistent length
     /// encoded to the header.
+    #[cfg_attr(
+        feature = "thiserror",
+        error("Insufficient buffer size for decrypted data")
+    )]
     TooShort,
 }
 
